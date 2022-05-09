f4308b77bd57931510b86c6d8eed19ecdf2aae41
Urs <urs@ursb.de>
2022-05-09 10:44:32 +0200

    new requirements.txt

--- app/routesfrontend.py
+++ app/routesfrontend.py
@@ -1,6 +1,7 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hallo Welt!"
+    return render_template('base.html',title="Test")
