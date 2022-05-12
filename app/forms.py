103609a7a8baf08260c61be904f8e48c077bb41c
Tom Herzberg <tom.herzberg@st.ovgu.de>
2022-05-12 15:10:37 +0200

    ohne rüberziehen

--- app/forms.py
+++ app/forms.py
@@ -4,13 +4,13 @@ from wtforms import SubmitField, SelectMultipleField
 
 # gibt nur parent directory, also app zurück und sucht in app nach ZUTATEN.txt
 zutatenPath = os.path.dirname(os.path.abspath(__file__)) + "\\ZUTATEN.txt"
-#zutatenListe = open(zutatenPath, 'r').readlines()
-zutatenListe = ["Ei", "Apfel"]
+zutatenListe = open(zutatenPath, 'r').readlines()
+#zutatenListe = ["Ei", "Apfel"]
 
 class d_felder(FlaskForm):
     global zutatenListe
     """ Swip Swap Formular"""
-    eingabe = SelectMultipleField('Zur Verfuegung stehende Objekte', choices=[])
+    eingabe = SelectMultipleField('Zur Verfuegung stehende Objekte', choices=zutatenListe)
     submit2 = SubmitField("-->")        # Hinzufuegen
     selected = SelectMultipleField('Ausgewaehlte Objekte', choices=[])
     submit3 = SubmitField("<--")        #Entfernen
