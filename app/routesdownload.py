5cf2e45b3dd9a917033664ad02e044d0d6a736e0
Urs Braun <urs@ursb.de>
2022-05-23 09:55:34 +0200

    ADD rezeptanzeige

--- app/routesdownload.py
+++ app/routesdownload.py
@@ -3,6 +3,11 @@ from app import app
 import os
 
 @app.route('/file/<path:name>')
-def bootstrapdev(name):
+def showfile(name):
+    path = os.path.dirname(os.path.realpath(__file__))
+    return send_from_directory(path+"/files/",name,as_attachment=False)
+
+@app.route('/file/<path:name>')
+def downfile(name):
     path = os.path.dirname(os.path.realpath(__file__))
     return send_from_directory(path+"/files/",name,as_attachment=True)
