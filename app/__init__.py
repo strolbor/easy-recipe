e6c2d3479f46dfa9d0790b92f8f646f94caa948d
Tom Herzberg <tom.herzberg@st.ovgu.de>
2022-05-30 10:45:29 +0200

    Classes

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
 from flask import Flask
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 #from flask_mysqldb import MySQL
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+class Config(object):
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app = Flask(__name__)
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 app.config["SECRET_KEY"] ="bhjewjvhewhewhje"
 app.config["MYSQL_HOST"] = ''
--- app/rezept.py
+++ app/rezept.py
@@ -1,9 +1,9 @@
-class rezeptvorschau:
 
-    def __init__(self, bildurl, titel, zutaten, tags, rezeptid):
+class rezept(db.Model):
+    zutaten = ""
+    def __init__(self, bildurl, titel, tags, rezeptid):
         self.bildurl = bildurl
         self.titel = titel
-        self.zutaten = zutaten
         self.tags = tags
         self.rezeptid = rezeptid
 
