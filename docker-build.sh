c833b48e903daae660b2d6387e9c7d9215add1ce
Urs-Benedict Braun <urs@ursb.de>
2022-08-06 17:30:25 +0200

    Zum Commit vorgemerkte Änderungen: 	geändert:       docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,9 @@
 sudo docker container rm -f er
 sudo docker build -t er .
 
-sudo docker run -p 5000:5000 --name er er &
+#sudo docker run -p 5000:5000 --name er er &
+#sudo docker run -d restart-always -p 5000:5000 --name easyrecipe er &
+sudo docker run --restart=always -p 5000:5000 --name er er &
 
 #sudo docker container prune -f
 #sudo docker image prune -a -f
