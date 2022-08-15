e2ee2e14f0755d0291c238f89c19744cb6ad0c24
Urs Braun <urs-benedict.braun@st.ovgu.de>
2022-08-15 09:31:03 +0200

    CHG: docker-build.sh #2

--- docker-build.sh
+++ docker-build.sh
@@ -1,6 +1,6 @@
-sudo docker container rm -f er
-sudo docker build -t er .
-sudo docker run --restart=always -p 5000:5000 --name er er &
+sudo docker container rm -f docker.ursb.de/easyrecipe
+sudo docker build -t docker.ursb.de/easyrecipe .
+sudo docker run --restart=always -p 5000:5000 --name er docker.ursb.de/easyrecipe &
 
 sudo docker rmi -f $(docker images --filter "dangling=true" -q --no-trunc)
 sudo docker container prune -f
