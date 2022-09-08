1bafdd5ac7e0294e5c73780d71944d10da3e5228
Urs Braun <urs-benedict.braun@dataport.de>
2022-09-08 08:10:12 +0200

    erster AJAX Test

--- app/routestmp.py
+++ app/routestmp.py
@@ -19,3 +19,14 @@ def tmpein():
         pass
 
     return render_template('nutzer_rezeptanlege.html', form=form)
+
+
+@app.route("/postskill", methods=["POST", "GET"])
+def postskill():
+    if request.method == 'POST':
+        skills = request.form.getlist('zutat[]')
+        for value in skills:
+            print(f"Erhalten: {value}")
+
+        msg = 'New record created successfully'
+    return str(msg)
